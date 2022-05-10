bcf5252af1c9d20ed5b5ef8ee66413d4d8c02831
Emmanuel Abreu <eabreue1@gmail.com>
2022-05-09 23:19:27 -0400

    reinitialized repo, setup webpack files, and messed around with pages elements

---
+++ src/resPageElements.js
@@ -0,0 +1,6 @@
+function createInitialPageElement(){
+    const resImg1 = document.createElement("img");
+    const headline = document.createElement("h1");
+
+    document.body.appendChild(resImg1, headline);
+}
